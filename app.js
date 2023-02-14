c86a0056d1d36be3989df1f8e4131e50ce448f2a
manuelecheandia <manuel.ep.10@gmail.com>
2023-02-14 11:25:43 -0400

    working with sorts and loops in frontend

--- app.js
+++ app.js
@@ -25,19 +25,51 @@ makes.unshift("Kia");
 
 console.log(makes);
 
-let myArray = [];
-myArray.push("Trudy");
-myArray.push("Miguel", "Stanley");
-myArray.push("Chris");
+let firstArray = [];
+firstArray.push("Trudy");
+firstArray.push("Miguel", "Stanley");
+firstArray.push("Chris");
 
-console.log(myArray);
+console.log(firstArray);
 
 let mySecondArray = ["Dylan", "Sara", "Adrian"];
 
-myArray.push(...mySecondArray);
+firstArray.push(...mySecondArray);
 
-console.log(myArray);
+console.log(firstArray);
 
-for (let i = 0; i < myArray.length; i++) {
-  console.log(`Name: ${myArray[i]}`);
+for (let i = 0; i < firstArray.length; i++) {
+  console.log(`Name: ${firstArray[i]}`);
 }
+
+for (let i in firstArray) {
+  console.log(`${firstArray[i]}`);
+}
+
+for (let n of firstArray) {
+  console.log(n);
+}
+
+let numbers = [2, 1, 13, 3, 45, 5, 100, 202, 20, 30, 50, 42];
+console.log(numbers);
+
+//alphanumeric sorting even with an array with true numbers
+numbers.sort();
+console.log(numbers);
+
+//To sort numerical , we need to provide a sort function to the sort method
+numbers.sort((x, y) => {
+  return x - y;
+});
+console.log(numbers);
+
+//sort by desc numerical order
+numbers.sort((a, b) => {
+  return b - a;
+});
+console.log(numbers);
+
+//Descending order of strings
+firstArray.sort().reverse();
+
+console.log(firstArray);
